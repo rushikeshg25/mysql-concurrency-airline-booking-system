c74c7fd4949ded27f6d3dd1e498b73ad485407b7
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2024-12-27 23:06:06 +0530

    feat: SQL script

---
+++ db.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE IF NOT EXISTS airline;
+
+USE airline;
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS flights (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    flight_name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS bookings (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    flight_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (flight_id) REFERENCES flights(id)
+);
